46f1747714162c9c97b6d8c46afd1f4e3cae0e38
fhidalgo <francochytatil@gmail.com>
2023-05-30 17:01:15 -0300

    video del main fixed

--- app/src/main/java/com/quantum/mq09/MainActivity.java
+++ app/src/main/java/com/quantum/mq09/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         //video
         videoview  = (VideoView)findViewById(R.id.videoPresentacion);
-        String uriPath = "android.resource://com.quantum.mq09/"+R.raw.inicio_isotipo2;
+        String uriPath = "android.resource://com.quantum.mq09/"+R.raw.inicio_isotipo;
         Uri uri = Uri.parse(uriPath);
         videoview.setVideoURI(uri);
         videoview.requestFocus();
